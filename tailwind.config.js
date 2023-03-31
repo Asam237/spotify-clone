022829851bc66b6f151275a97797044b5f91129a
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-03-31 07:17:02 +0100

    feat: update tailwinds config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,22 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          md: "4rem",
+          lg: "5rem",
+          xk: "5rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
